7065b0830575ffd4d8a3e9f188599a75c8901469
22bcs13257-animesh-kumar <animeshk923@proton.me>
2025-02-18 10:20:08 +0530

    Update merge-sorted-array.js

--- merge-sorted-array.js
+++ merge-sorted-array.js
@@ -1,9 +1,8 @@
 function merge(nums1, m, nums2, n) {
-    let p1 = m - 1; // Pointer for nums1
-    let p2 = n - 1; // Pointer for nums2
-    let p = m + n - 1; // Pointer for the end of the merged array
+    let p1 = m - 1;
+    let p2 = n - 1;
+    let p = m + n - 1; 
 
-    // Merge in reverse order
     while (p1 >= 0 && p2 >= 0) {
         if (nums1[p1] > nums2[p2]) {
             nums1[p] = nums1[p1];
@@ -15,7 +14,6 @@ function merge(nums1, m, nums2, n) {
         p--;
     }
 
-    // If there are remaining elements in nums2, copy them
     while (p2 >= 0) {
         nums1[p] = nums2[p2];
         p2--;
